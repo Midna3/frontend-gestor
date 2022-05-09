e854c61377e7d7995e455bd5ace348a5ecd3a3dd
unknown <marioleandro34@gmail.com>
2022-05-09 16:50:51 -0300

    feat: adding school screen, unfinished version

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Header() {
+  return (
+    <div style={{backgroundColor: 'black', width: '100%', height: 97}}>
+      ALOW
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from "react-dom/client";
+
 import App from './App';
+import { globalCss } from '@stitches/react';
+import Header from './components/Header';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement as Element);
+
+const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box'
+  },
+  '#root': {
+    height: '100%',
+    fontFamily: 'DM Sans',
+  },
+  'html': {
+    height: '100%',
+    fontFamily: 'DM Sans',
+  },
+  'body': {
+    height: '100%',
+    fontFamily: 'DM Sans',
+  }
+  
+});
+
+globalStyles();
+
+root.render(
   <React.StrictMode>
+    <Header/>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
