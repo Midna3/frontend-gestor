4926b05ff828ed1275b43e2553e52f75158fa002
Átila Rodrigues <atila.rodriguesal@gmail.com>
2022-05-10 14:30:27 -0300

    feat: create stitches.config.ts

---
+++ src/stitches.config.ts
@@ -0,0 +1,38 @@
+import { globalCss, createStitches } from '@stitches/react';
+
+export const globalStyles = globalCss({
+  '*': {
+    padding: 0,
+    margin: 0,
+    scrollBehavior: 'smooth',
+    fontFamily: 'DM Sans',
+  },
+  a: {
+    textDecoration: 'none',
+    padding: 0,
+    margin: 0,
+  },
+  'a:visited': {
+    textDecoration: 'none',
+    color: 'unset',
+  },
+});
+
+export const { styled, css, keyframes } = createStitches({
+  theme: {
+    space: {
+      1: '10px',
+      2: '20px',
+      3: '40px',
+    },
+    colors: {
+      blueBrandZero: '#42A7F2',
+      greenBrandZero: '#42A774',
+      blackZero: '#0A1128',
+      whiteZero: '#EDE7E3',
+    },
+  },
+  media: {
+    sm: '(max-width: 480px)',
+  },
+});
