65c3d9920cd5521f312dc2a8345542af417e08ca
LuanAccioly <acciolyluan@gmail.com>
2022-05-15 02:54:44 -0300

    feat: adding route to schoolComparison

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { globalStyles } from './stitches.config';
 import { Header } from './components/Header/Header';
 import { SchoolPage } from './pages/SchoolPage/SchoolPage';
 import { HomePage } from './pages/HomePage/HomePage';
+import { SchoolComparison } from './pages/SchoolComparison/SchoolComparison';
 
 function App() {
   globalStyles();
@@ -11,8 +12,12 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<SchoolComparison />} />
         <Route path="/school/:schoolId" element={<SchoolPage />} />
+        <Route
+          path="/school/comparison/:schoolId"
+          element={<SchoolComparison />}
+        />
       </Routes>
     </div>
   );
