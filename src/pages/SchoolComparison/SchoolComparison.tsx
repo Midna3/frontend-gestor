7b3e8a60578198fcbd887da5b444261387af769a
Átila Rodrigues <atila.rodriguesal@gmail.com>
2022-05-18 20:30:28 -0300

    feat: add search to second school in SchoolComparison

--- src/pages/SchoolComparison/SchoolComparison.tsx
+++ src/pages/SchoolComparison/SchoolComparison.tsx
@@ -1,163 +1,103 @@
-import 'react-circular-progressbar/dist/styles.css';
-
-import { container, graph, statistics } from './style';
-
-import { SchoolCard } from '../../components/SchoolCard/SchoolCard';
+import { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
+import SearchContext from '../../contexts/SearchContext';
+
+import { GraphInfo } from '../../types/GraphInfo';
 import { LineChart } from '../../components/LineChart/LineChart';
 
-type Infos = {
-  data: {
-    type: string;
-    id: 1;
-    attributes: {
-      name: string;
-      inep: number;
-      address: string;
-      ddd: string;
-      phone: string;
-      matriculas: number | null;
-      docentes: number | null;
-      year: number;
-      adm: string;
-      idebIniciais: {
-        mean: number;
-        projection: number;
-      };
-      idebFinais: {
-        mean: number;
-        projection: number;
-      };
-      ied: {
-        meanCategory: string;
-        mean: number;
-      };
-      icg: {
-        meanCategory: string;
-        mean: number;
-      };
-      afd: {
-        meanCategory: string;
-        mean: number;
-      };
-    };
-  };
-};
+import { SchoolCard } from '../../components/SchoolCard/SchoolCard';
+import { container, graph, statistics } from './style';
+import 'react-circular-progressbar/dist/styles.css';
+import { Flex } from '../../components/Flex/Flex';
+import { Line } from 'react-chartjs-2';
 
 export const SchoolComparison = () => {
   const params = useParams();
-  const [infos, setInfos] = useState<Infos | null>(null);
-  const [dataGraph, setDataGraph] = useState({
-    labels: [2017, 2018, 2019],
-    datasets: [
-      {
-        label: 'Matriculas',
-        data: [0, 0, 0],
-        backgroundColor: '#2D2FF0',
-        lineTension: 0.5,
-        borderColor: '#2D2FF0',
-        pointBorderColor: '#2D2FF0',
-        pointBorderWidth: 5,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: '#2D2FF0',
-        pointHoverBorderColor: '#2D2FF0',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-      },
-      {
-        label: 'Docentes',
-        data: [0, 0, 0],
-        backgroundColor: '#E80054',
-        lineTension: 0.5,
-        borderColor: '#E80054',
-        pointBorderColor: '#E80054',
-        pointBorderWidth: 5,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: '#E80054',
-        pointHoverBorderColor: '#E80054',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-      },
-    ],
-  });
+  const {
+    setSearchSecondSchool,
+    secondSchool,
+    setSecondSchool,
+    secondSchoolGraphicsData,
+    setSecondSchoolGraphicsData,
+  } = useContext(SearchContext);
+  setSearchSecondSchool(true);
+
+  const [infos, setInfos] = useState<GraphInfo | null>(null);
+  const [graphicsData, setGraphicsData] = useState<number[][]>([[0, 0, 0]]);
+  const [graphicsLabels, setGraphicsLabels] = useState<string[]>(['0']);
 
   useEffect(() => {
+    setSecondSchool(null);
+    setSecondSchoolGraphicsData([[0, 0, 0]]);
+
     (async function () {
       try {
         const { data } = await api.get(
           `panel/school/${params.schoolId}?year=2019`
         );
-        const d2018 = await api.get(
+        setInfos(data);
+
+        const dataFrom2018 = await api.get(
           `panel/school/${params.schoolId}?year=2018`
         );
-        const d2017 = await api.get(
+        const dataFrom2017 = await api.get(
           `panel/school/${params.schoolId}?year=2017`
         );
-        const years = [2017, 2018, 2019];
-        const matriculados = [
-          d2017.data.data.attributes.matriculas || 0,
-          d2018.data.data.attributes.matriculas || 0,
-          data.data.attributes.matriculas || 0,
+
+        const ied = [
+          dataFrom2017.data.data.attributes.ied.mean,
+          dataFrom2018.data.data.attributes.ied.mean,
+          data.data.attributes.ied.mean,
         ];
 
-        const docentes = [
-          d2017.data.data.attributes.docentes || 0,
-          d2018.data.data.attributes.docentes || 0,
-          data.data.attributes.docentes || 0,
+        const ird = [
+          dataFrom2017.data.data.attributes.ird.mean,
+          dataFrom2018.data.data.attributes.ird.mean,
+          data.data.attributes.ird.mean,
         ];
-        const newGraph = {
-          labels: years,
-          datasets: [
-            {
-              label: 'Matriculas',
-              data: matriculados,
-              backgroundColor: '#2D2FF0',
-              lineTension: 0.5,
-              borderColor: '#2D2FF0',
-              pointBorderColor: '#2D2FF0',
-              pointBorderWidth: 5,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: '#2D2FF0',
-              pointHoverBorderColor: '#2D2FF0',
-              pointHoverBorderWidth: 2,
-              pointRadius: 1,
-              pointHitRadius: 10,
-            },
-            {
-              label: 'Docentes',
-              data: docentes,
-              backgroundColor: '#E80054',
-              lineTension: 0.5,
-              borderColor: '#E80054',
-              pointBorderColor: '#E80054',
-              pointBorderWidth: 5,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: '#E80054',
-              pointHoverBorderColor: '#E80054',
-              pointHoverBorderWidth: 2,
-              pointRadius: 1,
-              pointHitRadius: 10,
-            },
-          ],
-        };
-        setDataGraph(newGraph);
-        setInfos(data);
+
+        const tdi = [
+          dataFrom2017.data.data.attributes.tdi.mean,
+          dataFrom2018.data.data.attributes.tdi.mean,
+          data.data.attributes.tdi.mean,
+        ];
+
+        const icg = [
+          dataFrom2017.data.data.attributes.icg.mean,
+          dataFrom2018.data.data.attributes.icg.mean,
+          data.data.attributes.icg.mean,
+        ];
+
+        const afd = [
+          dataFrom2017.data.data.attributes.afd.mean,
+          dataFrom2018.data.data.attributes.afd.mean,
+          data.data.attributes.afd.mean,
+        ];
+
+        const graphicsData = [ied, ird, tdi, icg, afd];
+        const graphicsLabels = [
+          'Índice de Esforço Docente',
+          'Indicador da Regularidade do Corpo Docente',
+          'Distorção idade série',
+          'Complexidade de Gestão Escolar',
+          'Indicador de Adequação da Formação Docente',
+        ];
+
+        setGraphicsData(graphicsData);
+        setGraphicsLabels(graphicsLabels);
       } catch (error) {
-        console.log('ERRO');
+        console.log(error);
       }
     })();
-    console.log(params.schoolId);
   }, [params.schoolId]);
+
   return (
     <div className={container()} style={{ backgroundColor: '#F1F4FA' }}>
       <div>
         <SchoolCard
           name={infos?.data.attributes.name || '----'}
-          inep={infos ? `#${infos?.data.attributes.inep}` : '----'}
+          inep={infos ? `${infos?.data.attributes.inep}` : '----'}
           phone={
             infos
               ? `(${
@@ -180,40 +120,107 @@ export const SchoolComparison = () => {
           }}
         />
         <div className={statistics()}>
-          <p>Estatisticas</p>
-          <h1>Matriculas e Docentes</h1>
-          <div className={graph()}>
-            <div style={{ width: '80%' }}>
-              <LineChart Data={dataGraph} />
-            </div>
-          </div>
+          <p>Estatistícas</p>
+          {graphicsData.map((data, index) => (
+            <Flex direction={'column'} gap={3} css={{ marginTop: '2rem' }}>
+              <h2>{graphicsLabels[index]}</h2>
+              <Line
+                key={index}
+                data={{
+                  labels: [2017, 2018, 2019],
+                  datasets: [
+                    {
+                      label: '',
+                      data: data,
+                      backgroundColor: '#2D2FF0',
+                      borderColor: '#2D2FF0',
+                      pointBorderColor: '#2D2FF0',
+                      pointBorderWidth: 5,
+                      pointHoverRadius: 5,
+                      pointHoverBackgroundColor: '#2D2FF0',
+                      pointHoverBorderColor: '#2D2FF0',
+                      pointHoverBorderWidth: 2,
+                      pointRadius: 1,
+                      pointHitRadius: 10,
+                    },
+                  ],
+                }}
+                options={{
+                  plugins: {
+                    legend: {
+                      display: false,
+                    },
+                  },
+                }}
+              />
+            </Flex>
+          ))}
         </div>
       </div>
 
       <div>
         <SchoolCard
-          name={'Escola Barros Carvalho'}
-          inep={'#666'}
-          phone={'(81) 3229-6512'}
-          adress={'Esquina com a casa de Satanás - Inferno'}
-          adm={'Estadual'}
+          name={secondSchool?.data.attributes.name || '----'}
+          inep={secondSchool ? `${secondSchool?.data.attributes.inep}` : '----'}
+          phone={
+            secondSchool
+              ? `(${
+                  secondSchool?.data.attributes.ddd
+                }) ${secondSchool?.data.attributes.phone.slice(
+                  0,
+                  4
+                )}-${secondSchool?.data.attributes.phone.slice(4, 8)}`
+              : '----'
+          }
+          adress={secondSchool?.data.attributes.address || '----'}
+          adm={secondSchool?.data.attributes.adm || '----'}
           idebIniciais={{
-            mean: 5,
-            projection: 7,
+            mean: secondSchool?.data.attributes.idebIniciais.mean || 0,
+            projection:
+              secondSchool?.data.attributes.idebIniciais.projection || 0,
           }}
           idebFinais={{
-            mean: 3,
-            projection: 5,
+            mean: secondSchool?.data.attributes.idebFinais.mean || 0,
+            projection:
+              secondSchool?.data.attributes.idebFinais.projection || 0,
           }}
         />
         <div className={statistics()}>
-          <p>Estatisticas</p>
-          <h1>Matriculas e Docentes</h1>
-          <div className={graph()}>
-            <div style={{ width: '80%' }}>
-              <LineChart Data={dataGraph} />
-            </div>
-          </div>
+          <p>Estatistícas</p>
+          {secondSchoolGraphicsData.map((data, index) => (
+            <Flex direction={'column'} gap={3} css={{ marginTop: '2rem' }}>
+              <h2>{graphicsLabels[index]}</h2>
+              <Line
+                key={index}
+                data={{
+                  labels: [2017, 2018, 2019],
+                  datasets: [
+                    {
+                      label: '',
+                      data: data,
+                      backgroundColor: '#2D2FF0',
+                      borderColor: '#2D2FF0',
+                      pointBorderColor: '#2D2FF0',
+                      pointBorderWidth: 5,
+                      pointHoverRadius: 5,
+                      pointHoverBackgroundColor: '#2D2FF0',
+                      pointHoverBorderColor: '#2D2FF0',
+                      pointHoverBorderWidth: 2,
+                      pointRadius: 1,
+                      pointHitRadius: 10,
+                    },
+                  ],
+                }}
+                options={{
+                  plugins: {
+                    legend: {
+                      display: false,
+                    },
+                  },
+                }}
+              />
+            </Flex>
+          ))}
         </div>
       </div>
     </div>
